cf295de7a51aa6c8752a92cef58eb594775adf67
kamran1137 <kami.c1301@gmail.com>
2023-12-21 16:37:07 +0500

    init

---
+++ sing in/src/App.jsx
@@ -0,0 +1,12 @@
+import Loginform from "./components/Loginform";
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Loginform/>
+    </div>
+  )
+}
+
+export default App
---
+++ sing in/src/components/Loginform.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Loginform.css"
+function Loginform() {
+  return (
+   <>
+<main className='container'>
+
+    <div className='login-left' >
+         
+    </div>
+    <div className='login-right'>
+
+    </div>
+</main>
+
+   </>
+   
+  )
+}
+
+export default Loginform
